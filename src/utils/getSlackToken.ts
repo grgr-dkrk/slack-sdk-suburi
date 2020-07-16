e2a55cffd97244f7287be99d4c672665ebdae174
grgr-dkrk <shayashi513@gmail.com>
2020-07-16 23:35:25 +0900

    SLACK_BOT_TOKEN -> SLACK_TOKEN

--- src/utils/getSlackToken.ts
+++ src/utils/getSlackToken.ts
@@ -2,7 +2,7 @@
  * Get Slack Token
  */
 export const getSlackToken = (): string => {
-  const SLACK_TOKEN = process.env.SLACK_BOT_TOKEN
+  const SLACK_TOKEN = process.env.SLACK_TOKEN
   if (!SLACK_TOKEN) throw new Error('SLACK_TOKEN must exist.')
   return SLACK_TOKEN
 }
