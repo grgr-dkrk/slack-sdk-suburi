e2114f4e38806387238b825f4b8455d9d1e4f61a
grgr-dkrk <shayashi513@gmail.com>
2020-07-20 10:50:28 +0900

    add env & add options

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import { startPrompt } from './utils/prompt'
 import { methods, getMethodLabels, invoke } from './methods'
+import * as minimist from 'minimist'
 
 const main = async (): Promise<void> => {
-  const methodLabel = process.argv[2]
-  if (methodLabel) {
-    await invoke(methods, methodLabel)
+  const options = minimist(process.argv.slice(2))
+  if (options['m']) {
+    await invoke(methods, options['m'])
     process.exit(0)
   }
   const label = await startPrompt(getMethodLabels(methods))
